d34fc6fb5efe784fca1d720f238281e76421b882
John Fulton <john@TechElevator.com>
2017-05-09 12:57:04 -0400

    M1W1D3-espressions-lecture

---
+++ M1W1D3-expressions-lecture/BooleanExpressions/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration statement for a variable named "result" of type boolean
+            bool result;
+
+
+
+
+            Console.WriteLine("****************************");
+            Console.WriteLine("*** COMPARISON OPERATORS ***");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+            
+            /*
+             ==  Equal to
+             !=  Not equal to
+            */
+
+            result = (2 == 2);
+            Console.WriteLine("2 == 2 : " + result);
+
+            result = (2 == 3);
+            Console.WriteLine("2 == 3 : " + result);
+
+            result = ('a' == 'a');
+            Console.WriteLine("'a' == 'a' : " + result);
+
+            result = ('a' == 'b');
+            Console.WriteLine("'a' == 'b' : " + result);
+
+            result = (2 != 2);
+            Console.WriteLine("2 != 2 : " + result);
+
+            result = (2 != 3);
+            Console.WriteLine("2 != 3 : " + result);
+
+            result = ('a' != 'a');
+            Console.WriteLine("'a' != 'a' : " + result);
+
+            result = ('a' != 'b');
+            Console.WriteLine("'a' != 'b' : " + result);
+
+            Console.WriteLine();
+
+
+
+
+            /*
+             >  Greater than
+             <  Less than
+             >= Greater than or equal to
+             <= Less than or equal to
+            */
+            result = (2 > 1);
+            Console.WriteLine("2 > 1 : " + result);
+
+            result = (2 < 2);
+            Console.WriteLine("2 < 2 : " + result);
+
+            result = (2 < 2);
+            Console.WriteLine("2 < 2 : " + result);
+
+            result = (2 <= 3);
+            Console.WriteLine("2 <= 3 : " + result);
+
+            result = (5 >= 5);
+            Console.WriteLine("5 >= 5 : " + result);
+
+            Console.WriteLine();
+
+
+
+
+            Console.WriteLine("*************************");
+            Console.WriteLine("*** LOGICAL OPERATORS ***");
+            Console.WriteLine("*************************");
+            Console.WriteLine();       
+
+            /*
+		     &&  AND
+		    ||  OR
+		    !  NOT
+		    ^  XOR  (exclusive OR)
+		    */
+            result = true && true;
+            Console.WriteLine("true && true : " + result);
+
+            result = true && false;
+            Console.WriteLine("true && false : " + result);
+
+            result = false && false;
+            Console.WriteLine("false && false : " + result);
+
+            result = true || true;
+            Console.WriteLine("true || true : " + result);
+
+            result = true || false;
+            Console.WriteLine("true || false : " + result);
+
+            result = false || false;
+            Console.WriteLine("false || false : " + result);
+
+            result = !true;
+            Console.WriteLine("!true : " + result);
+
+            result = !false;
+            Console.WriteLine("!false : " + result);
+
+            result = true ^ true;
+            Console.WriteLine("true ^ true : " + result);
+
+            result = true ^ false;
+            Console.WriteLine("true ^ false : " + result);
+
+            result = false ^ false;
+            Console.WriteLine("false ^ false : " + result);
+
+            Console.WriteLine();
+
+
+
+
+
+            Console.WriteLine("***************************");
+            Console.WriteLine("*** BOOLEAN EXPRESSIONS ***");
+            Console.WriteLine("***************************");
+            Console.WriteLine();
+
+            int value = 100;
+            result = (value > 200) || (value < 500);
+            Console.WriteLine("(value > 200) || (value < 500) : " + result);
+
+            result = (value > 200) && (value < 500);
+            Console.WriteLine("(value > 200) && (value < 500) : " + result);
+
+            result = !(value >= 300);
+            Console.WriteLine("!(value >= 300) : " + result);
+
+
+
+            
+        }
+
+    }
+}
---
+++ M1W1D3-expressions-lecture/ConditionalExpressions/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*** IF STATEMENTS ***");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            int numberOfPeople = 25;
+            int slicesOfCake = 22;
+
+            if (numberOfPeople <= slicesOfCake)
+            {
+                Console.WriteLine("Yummy! Cake!");
+            }
+            else
+            {
+                Console.WriteLine("Burn the building down...");
+            }
+
+            bool condition1 = true;
+            bool condition2 = true;
+            if (condition1)
+            {
+                Console.WriteLine("Condition 1 is true!");
+            }
+            else if (condition2)
+            {
+                Console.WriteLine("Condition 2 is true!");
+            }
+            else
+            {
+                Console.WriteLine("No conditions are true!");
+            }
+
+
+            bool staplerIsSwingline = false;
+            if (staplerIsSwingline)
+                Console.WriteLine("Better be red...");
+
+            Console.WriteLine("And we continue...");
+
+            if (numberOfPeople <= slicesOfCake)
+            {
+                Console.WriteLine("Yummy! Cake!");
+            }
+            else
+            {
+                Console.WriteLine("Walk away mumbling...");
+                Console.WriteLine("Burn the building down...");
+            }
+
+
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*** VARIABLE SCOPE ***");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            int firstVariable = 2;
+            if (firstVariable > 0)
+            {
+                int secondVariable = firstVariable;
+            }
+            //int thirdVariable = secondVariable * 2; // this will cause a compile error because secondVariable is not "in scope"
+            //int firstVariable = 3;  // this is a compiler error because we already have a variable named firstVariable in this scope
+
+
+            Console.WriteLine("*******************************");
+            Console.WriteLine("********** METHODS ************");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            bool burnItDown = ShouldBurnDownTheBuilding(14, 20);
+
+            
+        }
+
+        static bool ShouldBurnDownTheBuilding(int numberOfEmployees, int piecesOfCake)
+        {
+            if (numberOfEmployees > piecesOfCake)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+
+    }
+}
