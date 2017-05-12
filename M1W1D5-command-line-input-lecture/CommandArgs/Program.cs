5a1e43d9c0936e606c9f1084305293807eb8a610
John Fulton <john@TechElevator.com>
2017-05-12 07:51:28 -0400

    M1W1D5-command-line-input-lecture

---
+++ M1W1D5-command-line-input-lecture/CommandArgs/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Printing out each of the arg values
+            for (int i = 0; i < args.Length; i++)
+            {                
+                Console.WriteLine($"Argument i is {args[i]}");
+            }
+
+            //2. Print the correct command according to the arguments provided                        
+            //      Try -help
+            //      Try -ascii
+            //      Try -clear
+            //      Try -color {red | blue | yellow}
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch(args[i])
+                {
+                    case "-help":
+                        Console.WriteLine();
+                        Console.WriteLine("You asked to see the help");
+                        Console.WriteLine("This is a pretty simple program and there isn't much");
+                        Console.WriteLine("The commands you can submit are");
+                        Console.WriteLine("     -help           to display the help");
+                        Console.WriteLine("     -ascii          to create a nice ascii art");
+                        Console.WriteLine("     -clear          to clear the display");
+                        Console.WriteLine("     -color color    to change the color");
+                        Console.WriteLine();
+                        break;
+
+                    case "-ascii":
+                        Console.WriteLine();
+                        Console.WriteLine(@"  $$\                         $$\                       $$\                               $$\                         ");
+                        Console.WriteLine(@"  $$ |                        $$ |                      $$ |                              $$ |                        ");
+                        Console.WriteLine(@"$$$$$$\    $$$$$$\   $$$$$$$\ $$$$$$$\         $$$$$$\  $$ | $$$$$$\ $$\    $$\ $$$$$$\ $$$$$$\    $$$$$$\   $$$$$$\  ");
+                        Console.WriteLine(@"\_$$  _|  $$  __$$\ $$  _____|$$  __$$\       $$  __$$\ $$ |$$  __$$\\$$\  $$  |\____$$\\_$$  _|  $$  __$$\ $$  __$$\ ");
+                        Console.WriteLine(@"  $$ |    $$$$$$$$ |$$ /      $$ |  $$ |      $$$$$$$$ |$$ |$$$$$$$$ |\$$\$$  / $$$$$$$ | $$ |    $$ /  $$ |$$ |  \__|");
+                        Console.WriteLine(@"  $$ |$$\ $$   ____|$$ |      $$ |  $$ |      $$   ____|$$ |$$   ____| \$$$  / $$  __$$ | $$ |$$\ $$ |  $$ |$$ |      ");
+                        Console.WriteLine(@"  \$$$$  |\$$$$$$$\ \$$$$$$$\ $$ |  $$ |      \$$$$$$$\ $$ |\$$$$$$$\   \$  /  \$$$$$$$ | \$$$$  |\$$$$$$  |$$ |      ");
+                        Console.WriteLine(@"   \____/  \_______| \_______|\__|  \__|       \_______|\__| \_______|   \_/    \_______|  \____/  \______/ \__|      ");
+                        Console.WriteLine();
+                        break;
+
+                    case "-clear":
+                        Console.Clear();
+                        Console.WriteLine("CLEARED");
+                        Console.WriteLine();
+                        break;
+
+                    case "-color":
+                        string color = args[i + 1];                        
+                        if (color == "red")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;                           
+                        }
+                        else if (color == "blue")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                        }
+                        else if (color == "yellow")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        Console.Clear();
+                        break;                    
+                }
+            }
+                         
+            
+            
+        }
+    }
+}
---
+++ M1W1D5-command-line-input-lecture/ConsoleInput/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////
+            //1. Ask for a first name
+            /////////////////////////
+            Console.WriteLine("Please provide your first name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("You entered " + firstName);
+
+            /////////////////////////
+            //2. Ask for a first and last name
+            /////////////////////////
+            Console.WriteLine("Please provide another first name");
+            string secondName = Console.ReadLine();
+
+            Console.WriteLine("How about a last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {secondName} {lastName}!");
+
+            Console.WriteLine("Where do you live?");
+            string city = Console.ReadLine();
+
+            /////////////////////////
+            //3. Read in and print a message out how many times?
+            /////////////////////////
+            Console.WriteLine("What message do you want me to print out?");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("Ok, how many times?");
+            //int numberOfTimes = Console.ReadLine(); <-- results in a compile error, why??
+            string input = Console.ReadLine();
+            int numberOfTimesValue = int.Parse(input);
+
+            for (int i = 0; i < numberOfTimesValue; i++)
+            {
+                Console.WriteLine(message);
+            }
+
+            /////////////////////////
+            //4. Read in a convert the value answer to a boolean
+            /////////////////////////
+            Console.WriteLine("Was there bad traffic today? Enter TRUE/FALSE");
+            input = Console.ReadLine();
+            bool trafficValue = bool.Parse(input);
+
+            if (trafficValue)
+            {
+                Console.WriteLine($"No surprise, its {city}!");
+            }
+            else
+            {
+                Console.WriteLine("Maybe everyone stayed home");
+            }
+
+            /////////////////////////
+            //5. Read in and convert the answer to a decimal
+            /////////////////////////
+            Console.WriteLine("How many hours do you sleep per night on average?");
+            input = Console.ReadLine();
+            double sleepValue = double.Parse(input);
+
+            Console.WriteLine($"You said you only slept {sleepValue}");
+        }
+    }
+}
