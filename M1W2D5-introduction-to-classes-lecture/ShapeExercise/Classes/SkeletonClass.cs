b5bb3b89b032f33ec70d45c4420c0b7c1173631c
John Fulton <john@TechElevator.com>
2017-05-19 08:53:30 -0400

    M1W2D5-introduction-to-classs-lecture

---
+++ M1W2D5-introduction-to-classes-lecture/ShapeExercise/Classes/SkeletonClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace Definition
+// Uppercase and aligned with a module or general purpose it provides
+// Seperated by Periods like TechElevator.Classes
+namespace ProjectNamespace
+{
+    // Class declaration
+    // Naming Convention: Singular, PascalCase
+    public class SkeletonClass
+    {
+
+        // Field aka class variable
+        //  - can only be referenced within the class
+        //  - follows camelCase naming convention
+        public string classVariable;
+        
+
+        ///////////////
+        // CONSTRUCTORS
+        ///////////////
+        // Default Constructor (no arguments)
+        // Notes: 
+        //  - Must match class name
+        //  - Does not return anything
+        //  - Also called the empty constructor
+        public SkeletonClass()
+        {
+        }
+
+        // Constructor
+        //  - Must match class name
+        //  - Still does not return anything
+        //  - Must be different by taking different parameters        
+        public SkeletonClass(string requiredVariableValue)
+        {
+            classVariable = requiredVariableValue;
+        }
+
+
+        ///////////////
+        // METHODS
+        ///////////////
+        // Public Method
+        //  - methods are named with verbs (i.e. GetSomething, CalculateSomething, DoSomething, AddSomething)
+        //  - all methods have a return statement that ends it
+        //  - It must return whatever type is defined in the method signature
+        //  - void requires us just to return; (it means nothing)
+        //  - methods control access to class variables - "Fields" 
+        public void MethodDoSomething()
+        {
+            return;
+        }
+
+        
+        // Public Method 2
+        //  - additional public method
+        public int MethodDoSomethingElse()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ M1W2D5-introduction-to-classes-lecture/ShapeExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 1. Add a using statement
+
+
+namespace ShapeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("[1] Add a wall");
+                Console.WriteLine("[2] Calculate paint required (and Exit)");
+                Console.Write("Please choose >>> ");
+                string userChoice = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if(userChoice == "1")
+                {
+                    Console.Write("Enter wall height >>> ");
+                    int height = int.Parse(Console.ReadLine());
+                    Console.Write("Enter wall width >>> ");
+                    int width = int.Parse(Console.ReadLine());
+                    int area = height * width;
+
+                    Console.WriteLine("Added " + height + "x" + width + " wall - " + area + " square feet");
+                }
+                else if (userChoice == "2")
+                {
+                    // Here we need to sum up the areas of all walls that have been entered
+                    Console.WriteLine("Wall 1: 10x15 - 150 square feet"); // PROTOTYPE ONLY!!!
+                    Console.WriteLine("Wall 2: 10x15 - 150 square feet"); // PROTOTYPE ONLY!!!
+                    Console.WriteLine("Wall 3: 10x15 - 150 square feet"); // PROTOTYPE ONLY!!!
+                    Console.WriteLine("Wall 4: 10x15 - 150 square feet"); // PROTOTYPE ONLY!!!
+
+                    int totalArea = 600; // PROTOTYPE ONLY!!!
+                    Console.WriteLine("===============================");
+                    Console.WriteLine("Total Area: " + totalArea + " square feet");
+
+                    // 1 gallon of paint covers 400 square feet
+                    double gallonsRequired = (double)totalArea / 400;
+                    Console.WriteLine("Paint Required: " + gallonsRequired + " gallons");
+
+                    return;
+                }
+            }
+
+        }
+    }
+}
