ca6f147f0c712ca78fbbd519ccd1be37ae1085ad
John Fulton <john@TechElevator.com>
2017-06-11 17:40:53 -0400

    M2W6D1-dao-lecture

---
+++ M2W6D1-dao-lecture/WorldGeography/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGeography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorldGeographyCLI cli = new WorldGeographyCLI();
+            cli.RunCLI();
+        }
+
+    }
+}
