a00001aae8a88bf784636bfc0af15662b7ba37a0
John Fulton <john@TechElevator.com>
2017-06-21 08:34:19 -0400

    M3W1D3-views-part1-lecture

---
+++ M3W1D3-views-part1-lecture/ViewsPart1.Lecture/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsPart1.Lecture.Controllers
+{
+    /*
+    * Each Controller Action below responds to a request from a browser.
+    * If the user visits http://localhost/Home/HelloInternet then the
+    * HelloInternet Action will execute, returning the HelloInternet view.
+    */
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        // GET: Home/
+        public string Index()
+        {
+            return "Hello World!";
+        }              
+    
+        // GET: Home/Welcome
+        public ActionResult Welcome()
+        {
+            return View("Welcome");
+        }
+
+        // GET: Home/HelloInternet
+        public ActionResult HelloInternet()
+        {
+            return View("HelloInternet");
+        }
+
+        
+        // GET: Home/MetricToImperial
+        public ActionResult MetricToImperial()
+        {
+            return View("MetricToImperial");
+        }
+    }
+}
---
+++ M3W1D3-views-part1-lecture/ViewsPart1.Lecture/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewsPart1.Lecture.MvcApplication" Language="C#" %>
