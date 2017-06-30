70b3ce452d62920bfd85ebf6b481aaa9190c4081
John Fulton <john@TechElevator.com>
2017-06-29 22:43:30 -0400

    M3W2D5-validation-lecture

---
+++ M3W2D5-validation-lecture/FlyByNightBank.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyByNightBank.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /
+        // GET: Home/
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+        // GET: Home/WhyFlyByNight
+        public ActionResult WhyFlyByNight()
+        {
+            return View("WhyFlyByNight");
+        }
+        
+    }
+}
---
+++ M3W2D5-validation-lecture/FlyByNightBank.Web/DAL/SurveySqlDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlyByNightBank.Web.Models;
+using System.Data.SqlClient;
+
+namespace FlyByNightBank.Web.DAL
+{
+    public class SurveySqlDAL : ISurveyDAL
+    {
+        private string connectionString;
+        private const string SQL_InsertSurvey = "INSERT INTO customer_surveys VALUES (@customerName, @state, @customer_experience, @additional_feedback, @survey_submitted);";
+
+        public SurveySqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        public bool SaveSurvey(Survey newSurvey)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_InsertSurvey, conn);
+                    cmd.Parameters.AddWithValue("@customerName", newSurvey.CustomerName);
+                    cmd.Parameters.AddWithValue("@state", newSurvey.State);
+                    cmd.Parameters.AddWithValue("@customer_experience", newSurvey.CustomerExperienceLevel);
+                    cmd.Parameters.AddWithValue("@additional_feedback", newSurvey.AdditionalFeedback);
+                    cmd.Parameters.AddWithValue("@survey_submitted", DateTime.UtcNow);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch(SqlException ex)
+            {
+                // Error Logging that a problem occurred, don't show the user
+                throw;
+            }
+        }
+    }
+}
