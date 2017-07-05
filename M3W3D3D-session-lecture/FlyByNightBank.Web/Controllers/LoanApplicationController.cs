5e4e601e84dc6d54996bf1eb4fc0b53ebd049118
John Fulton <john@TechElevator.com>
2017-07-05 09:14:02 -0400

    Additional example

---
+++ M3W3D3D-session-lecture/FlyByNightBank.Web/Controllers/LoanApplicationController.cs
@@ -0,0 +1,191 @@
+﻿using FlyByNightBank.Web.DAL;
+using FlyByNightBank.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyByNightBank.Web.Controllers
+{
+    public class LoanApplicationController : Controller
+    {
+        private readonly IApplicationDAL applicationDal;
+
+        /*
+        * Our IApplicationDAL is injected through the constructor.        
+        */
+        public LoanApplicationController(IApplicationDAL applicationDal)
+        {
+            this.applicationDal = applicationDal;
+        }
+
+        // GET: LoanApplication/Index
+        public ActionResult Index()
+        {
+            // setup for test of temp data
+            // only set in Index action
+            Session["sessionName1"] = "sessionValue";
+            ViewBag.viewbagName1 = "viewbagValue";
+            ViewData["viewdataName1"] = "viewdataValue";
+            TempData["tempdataName1"] = "tempdataValue";
+
+            return View("Index");
+        }
+
+        // GET: LoanApplication/Start
+        public ActionResult Start()
+        {
+
+            // setup for test of temp data
+            // view contents
+            //string session_controller = Session["sessionName1"] as string;
+            //string viewbag_controller = ViewBag.viewbagName1 as string;
+            //string viewdata_controller = ViewData["viewdataName1"] as string;
+            //string tempdata_controller = TempData["tempdataName1"] as string;
+
+            //string session_view = Session["sessionName2"] as string;
+            //string viewbag_view = ViewBag.viewbagName2 as string;
+            //string viewdata_view = ViewData["viewdataName2"] as string;
+            //string tempdata_view = TempData["tempdataName2"] as string;
+
+
+            return View("Start");
+        }
+
+        // POST: LoanApplication/Start
+        [HttpPost]
+        public ActionResult Start(LoanApplication application)
+        {
+            // setup for test of temp data
+            // view contents
+            //string session_controller = Session["sessionName1"] as string;
+            //string viewbag_controller = ViewBag.viewbagName1 as string;
+            //string viewdata_controller = ViewData["viewdataName1"] as string;
+            //string tempdata_controller = TempData["tempdataName1"] as string;
+
+            //string session_view = Session["sessionName2"] as string;
+            //string viewbag_view = ViewBag.viewbagName2 as string;
+            //string viewdata_view = ViewData["viewdataName2"] as string;
+            //string tempdata_view = TempData["tempdataName2"] as string;
+
+            Session["Loan_Application"] = application;
+
+            return RedirectToAction("PersonalContactInformation");
+        }
+
+        // GET: LoanApplication/PersonalContactInformation
+        public ActionResult PersonalContactInformation()
+        {
+            return View("PersonalContactInformation");
+        }
+
+        // POST: LoanApplication/PersonalContactInformation
+        [HttpPost]
+        public ActionResult PersonalContactInformation(LoanApplication application)
+        {
+            LoanApplication existingApplication = Session["Loan_Application"] as LoanApplication;
+
+            if (existingApplication != null)
+            {
+                existingApplication.HomeAddress = application.HomeAddress;
+                existingApplication.HomeCity = application.HomeCity;
+                existingApplication.HomeState = application.HomeState;
+                existingApplication.HomePostalCode = application.HomePostalCode;
+                existingApplication.HomePhone = application.HomePhone;
+            }
+
+            Session["Loan_Application"] = existingApplication;
+
+            return RedirectToAction("WorkContactInformation");
+        }
+
+        // GET: LoanApplication/WorkContactInformation
+        public ActionResult WorkContactInformation()
+        {
+            return View("WorkContactInformation");
+        }
+
+        // POST: LoanApplication/WorkContactInformation
+        [HttpPost]
+        public ActionResult WorkContactInformation(LoanApplication application)
+        {
+
+            LoanApplication existingApplication = Session["Loan_Application"] as LoanApplication;
+
+            if (existingApplication != null)
+            {
+                existingApplication.JobTitle = application.JobTitle;
+                existingApplication.WorkAddress = application.WorkAddress;
+                existingApplication.WorkCity = application.WorkCity;
+                existingApplication.WorkState = application.WorkState;
+                existingApplication.WorkPostalCode = application.WorkPostalCode;
+                existingApplication.WorkPhone = application.WorkPhone;
+            }
+
+            Session["Loan_Application"] = existingApplication;
+
+            return RedirectToAction("FinancialInformation");
+        }
+
+        // GET: LoanApplication/FinancialInformation
+        public ActionResult FinancialInformation()
+        {
+            return View("FinancialInformation");
+        }
+
+        // POST: LoanApplication/FinancialInformation
+        [HttpPost]
+        public ActionResult FinancialInformation(LoanApplication application)
+        {
+
+            LoanApplication existingApplication = Session["Loan_Application"] as LoanApplication;
+
+            if (existingApplication != null)
+            {
+                existingApplication.NetWorth = application.NetWorth;
+                existingApplication.Reference1Name = application.Reference1Name;
+                existingApplication.Reference1Phone = application.Reference1Phone;
+            }
+
+            Session["Loan_Application"] = existingApplication;
+
+
+            return RedirectToAction("Review");
+        }
+
+        // GET: LoanApplication/Review
+        public ActionResult Review()
+        {
+            LoanApplication loanApplication = Session["Loan_Application"] as LoanApplication;
+
+            return View("Review", loanApplication);
+        }
+
+        // POST: LoanApplication/Review
+        [HttpPost]
+        public ActionResult Submit()
+
+        {
+            LoanApplication loanApplication = Session["Loan_Application"] as LoanApplication;
+            applicationDal.SubmitApplication(loanApplication);
+            Session["Loan_Application"] = loanApplication;
+            return RedirectToAction("Submitted");
+        }
+
+        // GET: Confirmation
+        public ActionResult Submitted()
+        {
+            LoanApplication loanApplication = Session["Loan_Application"] as LoanApplication;
+            if (loanApplication != null)
+            {
+                return View("Submitted", loanApplication);
+            }
+            else
+            {
+                return View("Start");
+            }
+        }
+
+    }
+}
---
+++ M3W3D3D-session-lecture/FlyByNightBank.Web/DAL/ApplicationSqlDAL.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlyByNightBank.Web.Models;
+using System.Data.SqlClient;
+
+namespace FlyByNightBank.Web.DAL
+{
+    public class ApplicationSqlDAL : IApplicationDAL
+    {
+        private const string SQL_InsertApplication = @"INSERT INTO loan_applications 
+            VALUES (@first_name, @last_name, @logon_username, @home_address, @home_city, @home_state, @home_postalCode,
+                    @home_phone, @job_title, @work_address, @work_city, @work_state, @work_postalCode, @work_phone,
+                    @net_worth, @reference1_name, @reference1_phone, @date_submitted); SELECT CAST(SCOPE_IDENTITY() as int);";
+        private readonly string connectionString;
+
+        public ApplicationSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        public bool SubmitApplication(LoanApplication newLoanApplication)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_InsertApplication, conn);
+                    cmd.Parameters.AddWithValue("@first_name", newLoanApplication.FirstName);
+                    cmd.Parameters.AddWithValue("@last_name", newLoanApplication.LastName);
+                    cmd.Parameters.AddWithValue("@logon_username", newLoanApplication.LogonUsername);
+
+                    cmd.Parameters.AddWithValue("@home_address", newLoanApplication.HomeAddress);
+                    cmd.Parameters.AddWithValue("@home_city", newLoanApplication.HomeCity);
+                    cmd.Parameters.AddWithValue("@home_state", newLoanApplication.HomeState);
+                    cmd.Parameters.AddWithValue("@home_postalCode", newLoanApplication.HomePostalCode);
+                    cmd.Parameters.AddWithValue("@home_phone", newLoanApplication.HomePhone);
+
+                    if (String.IsNullOrEmpty(newLoanApplication.JobTitle))
+                    {
+                        cmd.Parameters.AddWithValue("@job_title", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@job_title", newLoanApplication.JobTitle);
+                    }
+
+                    if (String.IsNullOrEmpty(newLoanApplication.WorkAddress))
+                    {
+                        cmd.Parameters.AddWithValue("@work_address", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@work_address", newLoanApplication.WorkAddress);
+                    }
+
+                    if (String.IsNullOrEmpty(newLoanApplication.WorkCity))
+                    {
+                        cmd.Parameters.AddWithValue("@work_city", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@work_city", newLoanApplication.WorkCity);
+                    }
+
+                    if (String.IsNullOrEmpty(newLoanApplication.WorkState))
+                    {
+                        cmd.Parameters.AddWithValue("@work_state", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@work_state", newLoanApplication.WorkState);
+                    }
+
+                    if (String.IsNullOrEmpty(newLoanApplication.WorkPostalCode))
+                    {
+                        cmd.Parameters.AddWithValue("@work_postalCode", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@work_postalCode", newLoanApplication.WorkPostalCode);
+                    }
+
+
+                    if (String.IsNullOrEmpty(newLoanApplication.WorkPhone))
+                    {
+                        cmd.Parameters.AddWithValue("@work_phone", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@work_phone", newLoanApplication.WorkPhone);
+                    }
+
+
+                    cmd.Parameters.AddWithValue("@net_worth", newLoanApplication.NetWorth);
+                    cmd.Parameters.AddWithValue("@reference1_name", newLoanApplication.Reference1Name);
+                    cmd.Parameters.AddWithValue("@reference1_phone", newLoanApplication.Reference1Phone);
+
+                    newLoanApplication.DateSubmitted = DateTime.UtcNow;
+                    cmd.Parameters.AddWithValue("@date_submitted", newLoanApplication.DateSubmitted);
+
+                    int newId = (int)cmd.ExecuteScalar();
+
+                    newLoanApplication.ApplicationId = newId;
+
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                // Log the Exception so that someone can be notified.
+                return false;
+            }
+        }
+    }
+}
