c41a94476ba2cf626334c5e280d2c7885f9e6d4b
John Fulton <john@TechElevator.com>
2017-07-28 08:31:44 -0400

    M5W1D5-Hash & Encrypt

---
+++ M5W1D5A-Hash/Hash/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("This is a test");
+            Console.WriteLine(Compute.Hash("This is a test"));
+
+            Console.Write("Enter phrase to be hashed: ");
+            string inputPhrase = Console.ReadLine();
+            Console.WriteLine(Compute.Hash(inputPhrase));
+
+            Console.WriteLine("---------------------------");
+
+            string clearText = "Now is the time...really!";
+            Console.WriteLine(clearText);
+
+            string cipherText = Compute.Encrypt(clearText);
+            Console.WriteLine(cipherText);
+
+            string result = Compute.Decrypt(cipherText);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
